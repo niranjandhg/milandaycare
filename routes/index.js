9f349916260bbbe99c4967fed23829f1a15f4d6a
niranjandhg <niranjandhg@gmail.com>
2025-04-10 22:01:37 -0600

    adding services

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../views/index.html')));
 router.get('/about', (req, res) => res.sendFile(path.join(__dirname, '../views/about.html')));
-router.get('/programs', (req, res) => res.sendFile(path.join(__dirname, '../views/programs.html')));
+router.get('/services', (req, res) => res.sendFile(path.join(__dirname, '../views/services.html')));
 router.get('/contact', (req, res) => res.sendFile(path.join(__dirname, '../views/contact.html')));
 
 module.exports = router;
