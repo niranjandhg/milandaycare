0c62780afbb81bf52cc4146562b793c0fa77916b
niranjandhg <niranjandhg@gmail.com>
2025-04-21 21:48:49 -0600

    added design and contact page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const routes = require('./routes');  // Import the routes
+const routes = require('./routes/index');  // Import the routes
 
 const app = express();
 const PORT = process.env.PORT || 3000;
